281ab90d4b0916a115573a0f37629e4d93f5cffc
pje335_NL <meulenthomas@gmail.com>
2016-05-01 01:13:07 +0200

    first update
    I added the functions. I have difficulties with the while loop…. I wil
    see tomorrow

--- chest.c
+++ chest.c
@@ -1,23 +1,101 @@
 #include <stdio.h>
 
-int main(void){
+int board[8][8],a=0,x,y,row,colum;
+char q;
 
-  int board[8][8],x,y;
-
+/*making a new clean*/
+void playboard(){
   for(x=0; x<8;x++){
     for(y=0; y<8;y++){
-      board[x][y] = 0;
-      }
-
+        board[x][y] = 0;
     }
 
+  }
+}
 
-
+/*printing the board*/
+void print(){
+  printf("\n");
     for(x=0; x<8;x++){
       for(y=0; y<8;y++){
         printf("%d \t",board[x][y]);
         }
         printf("\n");
       }
-    return 0;
+}
+
+
+void placement(row,colum){
+
+      board[row][colum] = 1;
+  for(x=0; x<8;x++){
+    if(board[row][x] != 1){
+      board[row][x] = 5;
+    }
+  }
+  for(x=0; x<8;x++){
+    if(board[x][colum] != 1){
+      board[x][colum] = 5;
+    }
+  }
+  print();
+}
+
+
+/*asking the user for input*/
+  void input(){
+    printf("Please enter the row: " );
+    scanf("%d",&row );
+    row -= 1;
+    printf("Please enter the coulum: " );
+    scanf("%d",&colum);
+    colum -= 1;
+
+    /*Checking if the input is valid*/
+
+    if(row < 0 || row > 7 || colum < 0 || row > 7){
+      printf("INPUT IS INVALID, IT IS OFF  THE  BOARD! Try again\n" );
+      input();
+    }
+    else if (board[row][colum] == 5){
+      printf("THIS POSITION HAS ALREADY BEEN CHOSEN. TRY AGAIN\n" );
+      input();
+    }
+    else if (board[row][colum] == 1){
+      printf("THIS POSITION IS NOT POSIBLE DUE TO A OTHER QUEEN. TRY AGAIN \n" );
+      input();
+    }
+    else{
+      placement(row,colum);
+    }
+
+  }
+
+
+
+
+
+
+
+int main(void){
+  playboard();
+  print();
+  while(a == 0){
+    a = 1;
+    printf("Do you want to contiunu? (Y/N):" );
+    scanf("%c",&q );
+    printf("\n" );
+    if (q == 'N' || q == 'n'  ){
+      a = 1;
+      printf("PROGRAM STOPPING\n" );
+    }
+    else if(q == 'Y' || q == 'y'  ) {
+      a = 0;
+      input();
+    }
+
+    }
+
+  return 0;
+
 }
